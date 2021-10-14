0e56a30fcc171b9fa84ead18c13df9fd3166f933
PeanutCar <91556671+PeanutCar@users.noreply.github.com>
2021-10-13 19:53:31 -0700

    removed test type

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -4,3 +4,21 @@
 // @file   This files defines the MyClass class.
 // @author AuthorName.
 // @since  x.x.x
+
+// Creating a word search game:
+  // Print a board with a distinct number of randomized letters
+    // Create a 12x12 grid
+  // Print a “word list” for the user
+    // Make a list of 6 randomized words from a pre-written word bank
+  // Randomly insert words from the list horizontally, vertically, and diagonally
+    // In the remaining spaces, fill each with a randomized letter
+  // Accept user input
+  // Validate user move
+    // Allow the user to use their mouse to interact with the page
+  // Draw a line through words on the board and on “word list”
+    // Allow use of the mosue to draw a line
+      // Line locked to 8 cardinal directions
+      // Whichever cells the line touches are considered part of the attempt
+        // If the letters selected match correctly, then answer is validated
+    // Repeat for all words
+  // Show “complete” and play confetti animation over the board when all words from list are crossed
