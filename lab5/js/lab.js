98013ffe0a781e91f1c9b48de55007d4124f2dac
PeanutCar <91556671+PeanutCar@users.noreply.github.com>
2021-10-18 21:00:51 -0700

    added lab 5 code.
    Pushing to show partner

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+ /*
+  * Author: Katana Parker <kmparker@ucsc.edu>
+  * Created: 18 October 2021
+  * License: Public Domain
+  */
+
+  // Define Variables
+  make = "Volkswagen";
+  model = "Jetta";
+  color = "Plain Milk";
+  year = "2013";
+
+  // calculate
+  age = 2021 - year;
+
+
+  // Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
