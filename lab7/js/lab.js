cb6e5db6c130d2405feb681283dc550326456774
PeanutCar <91556671+PeanutCar@users.noreply.github.com>
2021-10-25 21:01:01 -0700

    fixed naming. test for work on server

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,14 +5,14 @@
  */
 
 
-//reverseUserName - takes a string and returns the reverse of that string. Reverses the characters from an input.
+//sortsUserName - takes a string and returns the reverse of that string. Reverses the characters from an input.
 
-function reverseUserName() {
-  //userName - Produces a window that gets the user's name.
-  var userName = prompt("Enter name -", "Ai Hasaname");
+function sortsUserName() {
+  //userName - Produces a prompt window that gets the user's name.
+  var userName = window.prompt("Enter Name -", "Ai Hasaname");
   console.log("userName =", userName);
 
-  //make string into an array.
+  //make the received string into an array.
   var nameArray = userName.split('');
   console.log("nameArray =", nameArray);
 
@@ -27,7 +27,8 @@ function reverseUserName() {
   //these processes can also be done in a single line:
   //  userName.toLower().split("").sort.join.
   return nameSorted;
+
 }
 
 //output
-document.writeln("Thank you for participating in this Katana System test, ", reverseUserName(), "!", "</br>");
+document.writeln("Thank you for participating in this Katana System test, ", sortsUserName(), ".", "</br>");
