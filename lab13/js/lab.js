f4b5d64ca4a149777c88955c348c67e403a6e225
PeanutCar <91556671+PeanutCar@users.noreply.github.com>
2021-11-22 23:44:10 -0800

    done

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,48 +1,40 @@
 /*
- * Author: Katana Parker <kmparker@ucsc.edu>
- * Created: 22 November 2021
- * License: Public Domain
+ * Author: Vincent Wenzler, Katana Parker
+ *Created: 22 November 2021
+ *License: Public Domain
+ With help from Vivian Zheng with the loop function
  */
-//"Fizzbuzz Function"
-// Loop through numbers 1 to 200, listing them as you go
-// If the number is a multiple of 3, if should print "Fizz!"
-// If the number is a multiple of 5, it should print "Buzz!"
-// If the number is a multiple of 7, it should print "Boom!"
-// If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print "FizzBuzz!" and so on
+world = "";
 
-            //"Fizzbuzz Function"
-// Loop through numbers 1 to 200, listing them as you go
-for (var i=0; i<=200; i++) {
-// If the number is a multiple of 3, if should print "Fizz!"
-if(i % 3 === 0) {
-// If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print "FizzBuzz!" and so on
-if(i % 3 === 0&& i % 5 === 0&& i % 7 === 0){
-console.log("FizzBuzzBOOM!");
+ //FizzBuzz function
+ // print Fizz in log if multiple of 3
+ // print Buzz in log if multiple of 5
+ // print Boom in log if multiple of 7
+ //if multiple of more than one number, print the rest
+function fizzbuzzboom(str) {
+  var str = "";
+  for (i=1; i <=200; i++) {
+      temp = "" + i + ": ";
+      if (i % 3 == 0) {
+          temp += "Fizz";
+      }
+      if(i % 5 == 0) {
+          temp += "Buzz";
+      }
+      if(i % 7 == 0){
+          temp += "Boom";
+      }
+  // if value is above lngth of 5 adds ! to the end
+      if(temp.length > 5){
+        temp += "!";
+      }
+ // adds new line after every value
+      temp +="<br>";
+      str += temp;
+   }
+   $("#output").html(str)
 }
-else {
-console.log("Fizz!")
-}}
 
-// If the number is a multiple of 5, it should print "Buzz!"
-else if (i % 5 === 0) {
-if(i % 3 === 0&& i % 5 === 0){
-console.log("FizzBuzzBOOM!");
-}
-else {
-console.log("Buzz!!")
-}}
-
-// If the number is a multiple of 7, it should print "Boom!"
-else if (i % 7 === 0) {
-if(i % 3 === 0&& i % 5 === 0&& i % 7 === 0){
-console.log("FizzBuzzBOOM!");
-}
-else {
-console.log("Boom!!!")
-}}
-
-else {
-console.log(i);
-}
-document.getElementById("output").innerHTML = console.log()
-}
+$("#button").click(function(){
+  fizzbuzzboom(world);
+})
