542e24c4bfd4c98ad1fc5f110ce3727826c5ee3e
PeanutCar <91556671+PeanutCar@users.noreply.github.com>
2021-11-29 23:42:20 -0800

    did lab 15. it works as intended now

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,3 +3,36 @@
  *Created: 29 November 2021
  *License: Public Domain
  */
+
+ var myUrl = "http://numbersapi.com/11/30/date";
+
+//
+ function callAjax() {
+   console.log("click");
+   // Using the core $.ajax() method
+   $.ajax({
+       // API endpoint
+       url: myUrl,
+       // Any data to send
+       // data: { id: 123},
+       // POST or GET request
+       type: "GET",
+       // data type we expect back
+       /* dataType : "json", */
+   })
+   // If the request succeeds
+   // data is passed back
+   .done(function(data) {
+       console.log("Success:", data);
+       $("#output").html(JSON.stringify(data));
+       })
+
+   // If the request fails
+   .fail(function(request,error) {
+       console.log(request, error);
+
+ 	})
+
+ }
+
+ $("#activate").click(callAjax);
